a3b0c479a87036b3473f64865ffde45741d27f39
Olver Hernandez <olver.hernandez@gmail.com>
2024-10-29 05:50:57 -0500

    Fix error in woocomerce with columns is 0

--- modules/woocommerce/module.php
+++ modules/woocommerce/module.php
@@ -1600,6 +1600,13 @@ class Module extends Module_Base {
 		if ( isset( $settings['posts_per_page'] ) && isset( $settings['columns'] ) ) {
 			$settings['rows'] = ceil( $settings['posts_per_page'] / $settings['columns'] );
 		}
+		if ( isset( $settings['posts_per_page'], $settings['columns']) && is_numeric($settings['posts_per_page']) && is_numeric($settings['columns'])) {
+    		if ((int)$settings['columns'] > 0) {  
+        		$settings['rows'] = ceil($settings['posts_per_page'] / $settings['columns']);
+    		} else {
+                $settings['rows'] = 1;  
+    		}
+		}
 
 		$settings['paginate'] = 'yes';
 		$settings['allow_order'] = 'no';
