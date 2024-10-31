d8dfe5b2320dcff1b8f8646e1e0828a2f3d0a1f4
proelements <proelements.org@gmail.com>
2024-10-31 14:51:30 +0200

    v3.25.0

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'elementor/elementor-pro',
+        'pretty_version' => '3.25.x-dev',
+        'version' => '3.25.9999999.9999999-dev',
+        'reference' => '4863fbea7dbfb34eeed4241b995a7177da488124',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => false,
+    ),
+    'versions' => array(
+        'elementor/elementor-pro' => array(
+            'pretty_version' => '3.25.x-dev',
+            'version' => '3.25.9999999.9999999-dev',
+            'reference' => '4863fbea7dbfb34eeed4241b995a7177da488124',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
